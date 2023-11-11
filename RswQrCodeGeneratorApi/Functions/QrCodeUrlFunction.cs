becd0a48833405892d01cb1bdf4bddc5c5d2f548
Radim Smilek <smilek.radim@gmail.com>
2023-11-11 21:37:39 +0100

    First working memory stream version.

--- RswQrCodeGeneratorApi/Functions/QrCodeUrlFunction.cs
+++ RswQrCodeGeneratorApi/Functions/QrCodeUrlFunction.cs
@@ -15,7 +15,9 @@ using Newtonsoft.Json;
 using QRCoder;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
+using static System.Net.Mime.MediaTypeNames;
 using static QRCoder.PayloadGenerator;
 
 namespace RswQrCodeGeneratorApi.Functions
@@ -33,7 +35,7 @@ namespace RswQrCodeGeneratorApi.Functions
         [OpenApiOperation(operationId: "QrCodeUrl", tags: new[] { "url" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "url", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Url** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "image/jpeg", bodyType: typeof(byte[]), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "image/png", bodyType: typeof(byte[]), Description = "The OK response")]
         public IActionResult QrCodeUrl(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ExecutionContext context)
         {
@@ -49,6 +51,22 @@ namespace RswQrCodeGeneratorApi.Functions
             QRCode qrCode = new QRCode(qrCodeData);
             var qrCodeAsBitmap = qrCode.GetGraphic(10);
 
+            var memoryStream = new MemoryStream();
+            qrCodeAsBitmap.SaveAsPng(memoryStream, new PngEncoder()
+            {
+                BitDepth = PngBitDepth.Bit1,
+                ColorType = PngColorType.Grayscale
+            });
+            memoryStream.Position = 0;
+            return new FileStreamResult(memoryStream, "image/png");
+
+            //var fullFileName = @$"{context.FunctionAppDirectory}\___QrCodeImageTmp.jpg";
+            //qrCodeAsBitmap.SaveAsPng(fullFileName, new PngEncoder()
+            //{
+            //    BitDepth = PngBitDepth.Bit1,
+            //    ColorType = PngColorType.Grayscale
+            //});
+
             //var memoryStream = new MemoryStream();
             //qrCodeAsBitmap.SaveAsJpeg(memoryStream);
             //return new FileStreamResult(memoryStream, "image/jpeg");
@@ -56,12 +74,12 @@ namespace RswQrCodeGeneratorApi.Functions
             //var memoryStream = new MemoryStream();
             //return new FileStreamResult(memoryStream, "image/jpeg");
 
-            var fullFileName = @$"{context.FunctionAppDirectory}\_QrCodeImageTmp.jpg";
-            if (File.Exists(fullFileName))
-                File.Delete(fullFileName);
-            qrCodeAsBitmap.SaveAsJpeg(fullFileName);
-            var fileStream = File.OpenRead(fullFileName);
-            return new FileStreamResult(fileStream, "image/jpeg");
+            //var fullFileName = @$"{context.FunctionAppDirectory}\_QrCodeImageTmp.jpg";
+            //if (File.Exists(fullFileName))
+            //    File.Delete(fullFileName);
+            //qrCodeAsBitmap.SaveAsJpeg(fullFileName);
+            //var fileStream = File.OpenRead(fullFileName);
+            //return new FileStreamResult(fileStream, "image/jpeg");
         }
 
     }
