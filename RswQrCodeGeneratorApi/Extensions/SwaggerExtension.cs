27be7fab24e86941f2ecb29d9ef5ff43a24fb52b
Radim Smilek <smilek.radim@gmail.com>
2023-11-17 18:33:18 +0100

    Added Startup class.

---
+++ RswQrCodeGeneratorApi/Extensions/SwaggerExtension.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace RswQrCodeGeneratorApi.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            const string ApiGithubUrl = "https://github.com/rsmilek/RswQrCodeGeneratorApi";
+            const string ApiUrl = "https://RswQrCodeGeneratorApi.Azure.com";
+
+            services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+            {
+                var options = new OpenApiConfigurationOptions()
+                {
+                    Info = new OpenApiInfo()
+                    {
+                        Version = "1.0.0",
+                        Title = "Swagger RswQrCodeGeneratorApi",
+                        Description = $"This is a QR code generaor API designed by [{ApiUrl}]({ApiUrl}).",
+                        TermsOfService = new Uri(ApiGithubUrl),
+                        Contact = new OpenApiContact()
+                        {
+                            Name = "Radim Smilek",
+                            Email = "rsmilek@seznam.cz",
+                            Url = new Uri(ApiGithubUrl),
+                        },
+                        License = new OpenApiLicense()
+                        {
+                            Name = "MIT",
+                            Url = new Uri("http://opensource.org/licenses/MIT"),
+                        }
+                    },
+                    Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                    OpenApiVersion = OpenApiVersionType.V3,
+                    IncludeRequestingHostName = true,
+                    ForceHttps = false,
+                    ForceHttp = false,
+                };
+
+                return options;
+            });
+        }
+    }
+}
---
+++ RswQrCodeGeneratorApi/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RswQrCodeGeneratorApi.Extensions;
+
+[assembly: FunctionsStartup(typeof(RswQrCodeGeneratorApi.Startup))]
+
+namespace RswQrCodeGeneratorApi
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSwagger();
+        }
+    }
+}
