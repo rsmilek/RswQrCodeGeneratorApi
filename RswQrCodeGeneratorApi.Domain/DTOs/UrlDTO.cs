30474f160925a3b09629311ce9ba7803ddfc4672
Radim Smilek <smilek.radim@gmail.com>
2023-11-18 15:04:38 +0100

    Added QR Code Url function with requests with body.

--- RswQrCodeGeneratorApi.Domain/DTOs/UrlDTO.cs
+++ RswQrCodeGeneratorApi.Domain/DTOs/UrlDTO.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using System;
 
 namespace RswQrCodeGeneratorApi.Domain.DTO
 {
     public class UrlDTO
     {
+        [OpenApiProperty(Nullable = true, Default = "https://github.com/JPlenert/QRCoder-ImageSharp", Description = "The URL text value")]
         public string Url { get; set; }
     }
 }
--- RswQrCodeGeneratorApi/Functions/QrCodeUrlFunction.cs
+++ RswQrCodeGeneratorApi/Functions/QrCodeUrlFunction.cs
@@ -34,50 +34,35 @@ namespace RswQrCodeGeneratorApi.Functions
         public IActionResult QrCodeUrl(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ExecutionContext context)
         {
-            try
-            {
-                string url = req.Query["url"];
-
-                var stream = new PayloadGenerator.Url(url).GenerateQrCode().SaveAsPngToStream();
-
-                return new FileStreamResult(stream, "image/png");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Unhandled Exception log", null);
-                throw;
-            }
-        }
-
-        [FunctionName(nameof(QrCodeUrlAsync))]
-        [OpenApiOperation(operationId: nameof(QrCodeUrlAsync), tags: new[] { "url" })]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "url", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Url** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "image/png", bodyType: typeof(byte[]), Description = "The OK response")]
-        public async Task<IActionResult> QrCodeUrlAsync(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ExecutionContext context)
-        {
             string url = req.Query["url"];
+            if (url == null)
+                return new BadRequestObjectResult("Please pass capacity and hours in the request body");
 
-            var stream = await Task.Run(() => new PayloadGenerator.Url(url).GenerateQrCode().SaveAsPngToStream());
+            var stream = new PayloadGenerator.Url(url).GenerateQrCode().SaveAsPngToStream();
 
             return new FileStreamResult(stream, "image/png");
         }
 
-        //[FunctionName(nameof(QrCodeUrl))]
-        //[OpenApiOperation(operationId: nameof(QrCodeUrl), tags: new[] { "url" })]
+        [FunctionName(nameof(QrCodeUrlAsync))]
+        [OpenApiOperation(operationId: nameof(QrCodeUrlAsync))]
         //[OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        //[OpenApiParameter(name: "url", In = ParameterLocation.Query, Required = true, Type = typeof(UrlDTO), Description = "The **Url** parameter")]
-        //[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "image/png", bodyType: typeof(byte[]), Description = "The OK response")]
-        //public async Task<IActionResult> QrCodeUrl(
-        //    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ExecutionContext context)
-        //{
-        //    var body = new StreamReader(req.Body).ReadToEnd();
-        //    var urlDTO = JsonConvert.DeserializeObject<UrlDTO>(body);
-        //    var stream = new PayloadGenerator.Url(urlDTO.Url).GenerateQrCode().SaveAsPngToStream();
+        [OpenApiRequestBody("application/json", typeof(UrlDTO),
+            Description = $"JSON request body containing {nameof(UrlDTO)}")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "image/png", bodyType: typeof(byte[]),
+            Description = "The OK response containing a image/png result.")]
+        public async Task<IActionResult> QrCodeUrlAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "POST", Route = null)] HttpRequest req, ExecutionContext context)
+        {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            var urlDTO = JsonConvert.DeserializeObject<UrlDTO>(body);
+            if (urlDTO == null)
+                return new BadRequestObjectResult($"Please pass {nameof(UrlDTO)} in the request body");
 
-        //    return new FileStreamResult(stream, "image/png");
-        //}
+            var stream = new PayloadGenerator.Url(urlDTO.Url).GenerateQrCode().SaveAsPngToStream();
+
+            return new FileStreamResult(stream, "image/png");
+        }
+
     }
 }
 
